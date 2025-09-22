521735584f0665ab84a43977f28c070a0fdae643
Arte71 <familiaegitodutra@hotmail.com>
2025-09-22 03:00:34 -0300

    Pagina dinâmica
    Deixar a estrutura mais dinâmica para o futuro

--- Coisa do Exército/Angular/Autolicitacao/src/app/app.ts
+++ Coisa do Exército/Angular/Autolicitacao/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterModule, LoginComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ Coisa do Exército/Exemplo PHP/exemplo-php/logout.php
@@ -0,0 +1,13 @@
+
+<html>
+<head>
+    <title>Exemplo de Integração com gov.br em PHP</title>
+</head>
+<body>
+    <h1>Página de logout da minha aplicação</h1>
+    <p>Logout realizado com sucesso!</p>
+</body>
+
+
+
+
