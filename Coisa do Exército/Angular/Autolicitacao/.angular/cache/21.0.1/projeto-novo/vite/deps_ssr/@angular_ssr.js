ed82659116253c715038dd3e8144d036ed6d383c
felipegasanto <felipegabrielscontato@gmail.com>
2025-12-08 20:31:30 -0300

    quase bom

---
+++ Coisa do Exército/Angular/Autolicitacao/.angular/cache/21.0.1/projeto-novo/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,41 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-V7PE7BTL.js";
+import "./chunk-2KS3U2TF.js";
+import "./chunk-4HTF4GAF.js";
+import "./chunk-5ZLOF4X6.js";
+import "./chunk-VLGBBXKZ.js";
+import "./chunk-6XO65Z54.js";
+import "./chunk-H2ZOBFY6.js";
+import "./chunk-6VILRTX6.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
